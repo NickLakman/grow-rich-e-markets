680414d1f3a9482c02808100c3d9643470ab4b40
NickLakman <140800149+NickLakman@users.noreply.github.com>
2023-09-28 11:06:53 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,28 +1,39 @@
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-    for (let i = 0; i < 10; i++) {
-
-    });
-    }
-function main() {
-
-}
-    const data = generateRandomData();
-    return data;
-function generateRandomData() {
-        console.log("Random Number: " + item);
-main();
-    const data = [];
-    data.forEach(item => {
-}        for (int item : data) {
-        }
     public static void main(String[] args) {
-}
-    }
-public class RandomDataGenerator {
+
         for (int i = 0; i < 10; i++) {
-        }
         int[] data = new int[10];
+    }
+}
             data[i] = (int) (Math.random() * 100) + 1;
 
+        }
+public class RandomDataGenerator {
             System.out.println("Random Number: " + item);
+        }
+        for (int item : data) {    data = generate_random_data()
+
+import random
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    main()
+def main():
+def generate_random_data():
+    for item in data:
+        print(f"Random Number: {item}")
+
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    data = generate_random_data()
+def generate_random_data():
+
+
+        print(f"Random Number: {item}")
+    for item in data:
+import random
+if __name__ == "__main__":
+
+    return data
